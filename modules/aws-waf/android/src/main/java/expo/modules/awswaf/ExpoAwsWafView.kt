4c9c833da37699f37cda02fed8a2a573c3240eb5
Sean Herman <92595165+sherman1-rbi@users.noreply.github.com>
2024-06-28 10:45:15 -0400

    Basic Expo Module for AWS WAF (#2)
    The basic Expo Module boilerplate to prepare for integration of the AWS
    WAF SDK

---
+++ modules/aws-waf/android/src/main/java/expo/modules/awswaf/ExpoAwsWafView.kt
@@ -0,0 +1,7 @@
+package expo.modules.awswaf
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ExpoAwsWafView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
